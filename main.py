cc5b767f23feba1807db96c69de207670d687eac
rithvik2703-art <rithvik.2703@gmail.com>
2025-08-25 07:34:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ except FileNotFoundError:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://footballai.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
